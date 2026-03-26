cadd99dd514271c4f788f00712bb791a284e7ff9
Mario Medina <mariomedinam@gmail.com>
2026-03-26 13:49:48 -0500

    Integración de Flowbite-React: Se ha añadido la dependencia "flowbite-react" en package.json y se ha configurado en vite.config.js. Además, se han creado archivos de configuración y de inicialización para Flowbite-React, junto con un nuevo archivo .gitignore. Se ha actualizado index.css para incluir el plugin de Flowbite-React.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
+    flowbiteReact()
   ],
   resolve: {
     alias: {
